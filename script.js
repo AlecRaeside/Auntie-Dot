f2eae8d47e982cebf218e439e4095a52d5008d7a
Alec Raeside <alec@AlMac.local>
2012-02-16 22:28:24 +1100

    Auto flip beams

--- script.js
+++ script.js
@@ -36,9 +36,9 @@
 			$(this).toggleClass("highlight");
 			$(this).append("<span class='beam'></span>");
 		})
-		setTimeout(function() {
-			auntie_dot.addClass("start");
-		},1000);
+		setInterval(function() {
+			flipBeams();
+		},9000);
 		
 		auntie_dot.click(function() {
 			$(this).toggleClass("changed");
@@ -46,13 +46,15 @@
 	}
 	
 	function flipBeams() {
+		var is_flipped = $(".flipped").length > 0;
 		var rows=$("#auntie_dot .row");
 		var num_cols=rows.eq(0).children().length;
 		
 		for (var i=num_cols;i>0;i--) {
 			setTimeout(function(i) {
+				if (is_flipped) i = num_cols-i;
 				rows.children(":nth-child("+i+")").find(".beam").toggleClass("flipped");
-			},150*(num_cols-i),i);
+			},100*(num_cols-i),i);
 		}
 	}
 	
